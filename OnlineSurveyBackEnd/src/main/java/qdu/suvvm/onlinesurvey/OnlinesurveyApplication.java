2979d49e78eb2b726c5d42747d84c9dcd57bc72e
suvvm <915269675@qq.com>
2019-11-10 21:21:49 +0800

    创建后台工程

---
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplication.java
@@ -0,0 +1,13 @@
+package qdu.suvvm.onlinesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinesurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinesurveyApplication.class, args);
+    }
+
+}
---
+++ OnlineSurveyBackEnd/src/test/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package qdu.suvvm.onlinesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinesurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
