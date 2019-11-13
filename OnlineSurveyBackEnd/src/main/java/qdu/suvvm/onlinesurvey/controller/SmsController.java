7172a5af678e1cab9f252e3259704a5fd4f42cb2
suvvm <915269675@qq.com>
2019-11-13 20:08:34 +0800

    添加获取手机验证码的Controller

--- OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/SmsController.java
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/SmsController.java
@@ -1,8 +1,43 @@
 package qdu.suvvm.onlinesurvey.controller;
 
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 /**
  * @Author: SUVVM
  * @Date: 2019/11/13 19:17
  */
+@RestController
 public class SmsController {
+    @PostMapping(value = "/getSms")
+    public String test(@RequestParam("mobile") String mobile, @RequestParam("code") String code) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "yourAccessKid", "yourSecret");
+        IAcsClient client = new DefaultAcsClient(profile);
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", mobile);
+        request.putQueryParameter("SignName", "suvvm");
+        request.putQueryParameter("TemplateCode", "SMS_172602858");
+        request.putQueryParameter("TemplateParam", code);
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            Map map = (Map)JSON
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
 }
