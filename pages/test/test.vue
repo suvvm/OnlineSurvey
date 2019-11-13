f21a5138ca6e47dda7eea590b08e48dd4e637416
suvvm <915269675@qq.com>
2019-11-13 14:34:51 +0800

    完成前后端通信测试代码

--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		
+		<van-button type="default"  @click="testSubmit">发送测试信息</van-button>
 	</view>
 </template>
 
@@ -12,7 +12,30 @@
 			}
 		},
 		methods: {
-			
+			testSubmit() {
+				var rp = require('request-promise');
+				var options = {
+				    method: 'POST',
+				    uri: 'http://localhost:8080/test',
+				    form: {
+				        // Like <input type="text" name="name">
+				        text1: 'showText1',
+						text2: 'showText2'
+				    },
+				    headers: {
+				        /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
+				    }
+				};
+				rp(options)
+				    .then(res => {
+				        // POST succeeded...
+						console.log(res)
+				    })
+				    .catch(err => {
+				        // POST failed...
+						console.log(err)
+				    });
+			}
 		}
 	}
 </script>
