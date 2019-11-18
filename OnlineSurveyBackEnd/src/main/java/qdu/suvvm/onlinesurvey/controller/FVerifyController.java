a5ca6cdbe284684b9f042cb60dabc134bb47d68a
suvvm <915269675@qq.com>
2019-11-18 22:08:13 +0800

    创建用于处理人脸识别请求的Controller FVerifyController

---
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/FVerifyController.java
@@ -0,0 +1,10 @@
+package qdu.suvvm.onlinesurvey.controller;
+
+/**
+ * @ClassName: FVerifyController
+ * @Description: TODO
+ * @Author: SUVVM
+ * @Date: 2019/11/18 22:06
+ */
+public class FVerifyController {
+}
