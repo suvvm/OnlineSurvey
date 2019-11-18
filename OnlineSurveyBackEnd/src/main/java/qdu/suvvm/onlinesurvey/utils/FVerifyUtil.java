b54ba1f4cd1592988729b82b653fa8fb91334d74
suvvm <915269675@qq.com>
2019-11-18 22:30:12 +0800

    完成人脸识别配置工具 FVerifyUtil

--- OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/utils/FVerifyUtil.java
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/utils/FVerifyUtil.java
@@ -1,5 +1,21 @@
 package qdu.suvvm.onlinesurvey.utils;
 
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * @ClassName: FVerifyUtil
  * @Description: TODO
@@ -7,4 +23,112 @@ package qdu.suvvm.onlinesurvey.utils;
  * @Date: 2019/11/18 22:22
  */
 public class FVerifyUtil {
+    public static String MD5Base64(String s) {
+        if (s == null)
+            return null;
+        String encodeStr = "";
+        byte[] utfBytes = s.getBytes();
+        MessageDigest mdTemp;
+        try {
+            mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(utfBytes);
+            byte[] md5Bytes = mdTemp.digest();
+            BASE64Encoder b64Encoder = new BASE64Encoder();
+            encodeStr = b64Encoder.encode(md5Bytes);
+        } catch (Exception e) {
+            throw new Error("Failed to generate MD5 : " + e.getMessage());
+        }
+        return encodeStr;
+    }
+
+    public static String HMACSha1(String data, String key) {
+        String result;
+        try {
+            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(signingKey);
+            byte[] rawHmac = mac.doFinal(data.getBytes());
+            result = (new BASE64Encoder()).encode(rawHmac);
+        } catch (Exception e) {
+            throw new Error("Failed to generate HMAC : " + e.getMessage());
+        }
+        return result;
+    }
+
+    public static String toGMTString(Date date) {
+        SimpleDateFormat df = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.UK);
+        df.setTimeZone(new java.util.SimpleTimeZone(0, "GMT"));
+        return df.format(date);
+    }
+    public static String sendPost(String url, String body, String ak_id, String ak_secret) throws Exception {
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        String result = "";
+        int statusCode = 200;
+        try {
+            URL realUrl = new URL(url);
+            /*
+             * http header 参数
+             */
+            String method = "POST";
+            String accept = "application/json";
+            String content_type = "application/json";
+            String path = realUrl.getFile();
+            String date = toGMTString(new Date());
+            // 1.对body做MD5+BASE64加密
+            String bodyMd5 = MD5Base64(body);
+            String stringToSign = method + "\n" + accept + "\n" + bodyMd5 + "\n" + content_type + "\n" + date + "\n"
+                    + path;
+            // 2.计算 HMAC-SHA1
+            String signature = HMACSha1(stringToSign, ak_secret);
+            // 3.得到 authorization header
+            String authHeader = "Dataplus " + ak_id + ":" + signature;
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", accept);
+            conn.setRequestProperty("content-type", content_type);
+            conn.setRequestProperty("date", date);
+            conn.setRequestProperty("Authorization", authHeader);
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(body);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            statusCode = ((HttpURLConnection)conn).getResponseCode();
+            if(statusCode != 200) {
+                in = new BufferedReader(new InputStreamReader(((HttpURLConnection)conn).getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            }
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (statusCode != 200) {
+            throw new IOException("\nHttp StatusCode: "+ statusCode + "\nErrorMessage: " + result);
+        }
+        return result;
+    }
+
 }
