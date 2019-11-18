f70bb4fde180ca32f358281a1d51f304db17cc65
suvvm <915269675@qq.com>
2019-11-18 09:01:09 +0800

    添加UserController

---
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/UserController.java
@@ -0,0 +1,10 @@
+package qdu.suvvm.onlinesurvey.controller;
+
+/**
+ * @ClassName: UserController
+ * @Description: TODO
+ * @Author: SUVVM
+ * @Date: 2019/11/18 8:57
+ */
+public class UserController {
+}
