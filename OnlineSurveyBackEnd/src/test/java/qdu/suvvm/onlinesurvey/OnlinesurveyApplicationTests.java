c68b5ffcdae21a854e6d3ad2f8426d1b6630e998
suvvm <915269675@qq.com>
2019-11-18 09:02:47 +0800

    编写数据库测试模块

--- OnlineSurveyBackEnd/src/test/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplicationTests.java
+++ OnlineSurveyBackEnd/src/test/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplicationTests.java
@@ -1,13 +1,34 @@
 package qdu.suvvm.onlinesurvey;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import qdu.suvvm.onlinesurvey.mapper.TagMapper;
+import qdu.suvvm.onlinesurvey.pojo.Tag;
 
+import java.util.List;
+
+@MapperScan(value = "qdu.suvvm.onlinesurvey.mapper")
 @SpringBootTest
 class OnlinesurveyApplicationTests {
 
+    @Autowired
+    private TagMapper tagMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void select() {
+        Tag t = new Tag();
+        // t.setId(400000001);
+        t.setName("秃头");
+        List<Tag> tags = tagMapper.selectTags(t);
+        for(Tag tag : tags){
+            System.out.println(tag.getId() + " " + tag.getName() + " " + tag.getDescription());
+        }
     }
 
 }
