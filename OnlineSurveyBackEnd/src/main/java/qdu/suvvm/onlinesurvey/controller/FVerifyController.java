98d72914fcd9ae77632a5a8fd79ffa4289943ff3
suvvm <915269675@qq.com>
2019-11-19 16:01:42 +0800

    FVerifyController.java人脸识别时将一并把用户详细数据返回给前端

--- OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/FVerifyController.java
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/FVerifyController.java
@@ -1,6 +1,10 @@
 package qdu.suvvm.onlinesurvey.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import qdu.suvvm.onlinesurvey.mapper.UserMapper;
@@ -20,11 +24,16 @@ public class FVerifyController {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${privateInfo.accessKeyId}")
+    private String ak_id;
+
+    @Value("${privateInfo.accessKeySecret}")
+    private String ak_secret;
+
+    @PostMapping(value = "/fVerify")
     public String FVerify(@RequestParam("pnum") String pnum, @RequestParam("content_1") String content_1) {
         int type = 1;
         String url = "https://dtplus-cn-shanghai.data.aliyuncs.com/face/verify";
-        String ak_id = "yourAccessKid";
-        String ak_secret = "yourSecret";
         User user = new User();
         user.setPnum(pnum);
         List<User> users = userMapper.getUser(user);
@@ -34,6 +43,16 @@ public class FVerifyController {
             String res = null;
             try {
                 res = FVerifyUtil.sendPost(url, body, ak_id, ak_secret);
+                JSONObject resObj = JSON.parseObject(res);
+                resObj.put("id", users.get(0).getId());
+                resObj.put("username", users.get(0).getUsername());
+                resObj.put("name", users.get(0).getName());
+                resObj.put("pnum", users.get(0).getPnum());
+                resObj.put("email", users.get(0).getEmail());
+                resObj.put("gender", users.get(0).getGender());
+                // resObj.put("avatar", users.get(0).getAvatar());
+                resObj.put("power", users.get(0).getPower());
+                res = JSON.toJSONString(resObj);
                 return res;
             } catch (Exception e) {
                 e.printStackTrace();
