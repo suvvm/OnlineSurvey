3b12ac6d6e9ae4dc9de0e977ff1a91b90c17ff82
suvvm <915269675@qq.com>
2019-11-19 22:22:19 +0800

    修改description

--- OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/UserController.java
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/UserController.java
@@ -1,16 +1,19 @@
 package qdu.suvvm.onlinesurvey.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import qdu.suvvm.onlinesurvey.mapper.UserMapper;
 import qdu.suvvm.onlinesurvey.pojo.User;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @ClassName: UserController
- * @Description: TODO
+ * @Description: 处理关于用户的相关请求
  * @Author: SUVVM
  * @Date: 2019/11/18 8:57
  */
@@ -38,5 +41,39 @@ public class UserController {
             return "success";
         }
         return "error";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        User user = new User();
+        user.setUsername(username); // 以传入的信息作为用户名查询用户
+        user.setPassword(password);
+        List<User> users = userMapper.getUser(user);
+        if (users.isEmpty()) {  // 以传入的信息作为电话查询用户
+            user.setUsername(null);
+            user.setPnum(username);
+            users = userMapper.getUser(user);
+            if (users.isEmpty()) {  // 以传入的信息作为邮箱查询用户
+                user.setPnum(null);
+                user.setEmail(username);
+                users = userMapper.getUser(user);
+                if (users.isEmpty()) {
+                    return "null";
+                }
+            }
+        }
+
+        // 创建JSONObject将查询到的用户数据都装入JSON对象
+        JSONObject resObj = new JSONObject();
+        resObj.put("id", users.get(0).getId());
+        resObj.put("username", users.get(0).getUsername());
+        resObj.put("name", users.get(0).getName());
+        resObj.put("email", users.get(0).getEmail());
+        resObj.put("gender", users.get(0).getGender().toString());
+        resObj.put("power", users.get(0).getPower());
+        // resObj.put("avatar", users.get(0).getAvatar());
+
+        // 将JSON对象转为JSON串发送给前端
+        return resObj.toJSONString();
     }
 }
