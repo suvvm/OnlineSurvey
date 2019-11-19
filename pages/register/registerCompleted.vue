0ea2c112bd9fe28bcd74f6251a980fad88e63b65
suvvm <915269675@qq.com>
2019-11-19 22:29:07 +0800

    添加注册完成页

---
+++ pages/register/registerCompleted.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<van-steps :active="active">
+			<van-step>注册账号</van-step>
+			<van-step>选择角色</van-step>
+			<van-step>等待确认</van-step>
+			<van-step>注册完成</van-step>
+		</van-steps>
+		
+		<van-button class="nex" type="info" size="large" @click="submit">立刻体验</van-button>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				active: 3,
+				userInfo: {},
+			}
+		},
+		onLoad() {
+			if(this.$cookies.get("userInfo") != null){
+				this.userInfo = this.$cookies.get("userInfo");
+			}
+		},
+		methods: {
+			submit() {
+				this.$router.push({
+					path: '/pages/user/user', 
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.nex{
+		margin-top: 100rpx;
+	}
+</style>
