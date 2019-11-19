9de65f511847bf7712420f031551deba6901ef6e
suvvm <915269675@qq.com>
2019-11-19 22:26:18 +0800

    封装MD5加密工具

---
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/utils/MD5.java
@@ -0,0 +1,29 @@
+package qdu.suvvm.onlinesurvey.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @ClassName: MD5
+ * @Description: TODO
+ * @Author: SUVVM
+ * @Date: 2019/11/19 19:24
+ */
+public class MD5 {
+    public static String getMd5(String input)
+    {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
