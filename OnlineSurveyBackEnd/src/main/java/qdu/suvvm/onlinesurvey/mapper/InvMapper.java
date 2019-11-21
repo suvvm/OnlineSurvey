5d82a0a1db87451e2212f11ed7c9fce847f47b57
suvvm <915269675@qq.com>
2019-11-21 22:07:18 +0800

    添加针对investigates的数据访问的InvMapper

---
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/mapper/InvMapper.java
@@ -0,0 +1,93 @@
+package qdu.suvvm.onlinesurvey.mapper;
+
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.jdbc.SQL;
+import qdu.suvvm.onlinesurvey.pojo.Investigate;
+
+import java.util.List;
+
+/**
+ * @ClassName: InvMapper
+ * @Description: 针对investigates的数据访问方法
+ * @Author: SUVVM
+ * @Date: 2019/11/21 21:38
+ */
+public interface InvMapper {
+
+    // 根据动态sql查询investigate
+    @SelectProvider(type = InvMapperProvider.class, method = "findInv")
+    public List<Investigate> gatInvestigate(Investigate inv);
+
+    // 根据id删除investigate
+    @Delete("delete from investigates where id=#{id}")
+    public int deleteInvById(Integer id);
+
+    // 插入investigate并获取自增id
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into investigates(name,visible,description,details,ownerid,number,time) values(#{name},#{visible},#{description},#{details},#{owner.id},#{number},#{time})")
+    public int insertInvestigate(Investigate inv);
+
+    // 根据id更新investigate
+    @UpdateProvider(type = InvMapperProvider.class, method = "updateInv")
+    public int updateInvestigate(Investigate inv);
+
+    class InvMapperProvider{
+        /**
+         * @FunctionName: findInv
+         * @Description: 用于生成查询问卷的动态sql
+         * @Parameter:
+         *  inv 查询数据封装的问卷类
+         * @Return: 返回对应sql语句
+         */
+        public String findInv(Investigate inv) {
+            return new SQL() {
+                {
+                    SELECT("*");
+                    FROM("investigates");
+                    if(inv.getId() != null) {
+                        WHERE("id = #{id}");
+                    }
+                    if(inv.getName() != null) {
+                        WHERE("name=#{name}");
+                    }
+                    if(inv.getOwner() != null){
+                        WHERE("ownerid=#{owner.id}");
+                    }
+                }
+            }.toString();
+        }
+        /**
+         * @FunctionName: updateInv
+         * @Description: 用于生成更新问卷的动态sql
+         * @Parameter:
+         *  cmp 更新数据封装的问卷类
+         * @Return: 返回对应sql语句
+         */
+        public String updateInv(Investigate inv) {
+            return new SQL() {
+                {
+                    UPDATE("companies");
+                    if(inv.getName() != null) {
+                        SET("name = #{name}");
+                    }
+                    if(inv.getDescription() != null) {
+                        SET("description = #{description}");
+                    }
+                    if(inv.getVisible() != null) {
+                        SET("visible = #{visible}");
+                    }
+                    if(inv.getDetails() != null) {
+                        SET("details = #{details}");
+                    }
+                    if(inv.getNumber() != null) {
+                        SET("number = #{number}");
+                    }
+                    if(inv.getTime() != null) {
+                        SET("time = #{time}");
+                    }
+                    WHERE("id = #{id}");
+                }
+            }.toString();
+        }
+    }
+}
