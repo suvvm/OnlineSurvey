2a3a79e9aec414b70938dd5dcf0528f7aa5053a1
suvvm <915269675@qq.com>
2019-11-21 20:45:16 +0800

    修改测试信息

--- OnlineSurveyBackEnd/src/test/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplicationTests.java
+++ OnlineSurveyBackEnd/src/test/java/qdu/suvvm/onlinesurvey/OnlinesurveyApplicationTests.java
@@ -49,6 +49,15 @@ class OnlinesurveyApplicationTests {
     }
 
     @Test
+    public void insertTag() {
+        Tag t = new Tag();
+        t.setName("体罚");
+        t.setDescription("听说，你在训练中有打骂体罚战士的现象？ 有。 好样的！");
+        int res = tagMapper.insertTag(t);
+        System.out.println(res);
+    }
+
+    @Test
     public void selectUser() {
         User u = new User();
         u.setName("江民民");
@@ -59,12 +68,17 @@ class OnlinesurveyApplicationTests {
     }
 
     @Test
-    public void insertTag() {
-        Tag t = new Tag();
-        t.setName("体罚");
-        t.setDescription("听说，你在训练中有打骂体罚战士的现象？ 有。 好样的！");
-        int res = tagMapper.insertTag(t);
-        System.out.println(res);
+    public void insertUserTag() {
+        int res = userMapper.insertUserTag(100000003,400000005);
+        if (res > 0) {
+            System.out.println("success");
+        }
+        User u = new User();
+        u.setId(100000003);
+        List<User> users = userMapper.getUser(u);
+        for(User user : users) {
+            System.out.println(user.toString());
+        }
     }
 
     /**
