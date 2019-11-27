b5616df8046f7fabdfb2b17658d972738e347f84
suvvm <915269675@qq.com>
2019-11-27 22:26:17 +0800

    verifyInvestigates 添加问卷显示功能

--- pages/investigates/verifyInvestigates.vue
+++ pages/investigates/verifyInvestigates.vue
@@ -1,7 +1,45 @@
 <template>
+	<view>
+		<van-cell v-for="(item, key) in invList" v-bind:key="key" :title="item.name" :value="item.id" size="large" :label="item.description" clickable/>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				invList: []
+			}
+		},
+		onLoad() {
+			this.$toast.loading({
+				duration: 0,	// 持续展示 toast
+				forbidClick: true,	// 禁用背景点击
+				message: '加载中'
+			});
+			
+			if(this.$cookies.get("userInfo") != null){	// 获取当前用户信息
+				this.userInfo = this.$cookies.get("userInfo");
+			}
+			var rp = require('request-promise');
+			// 获取所有tag
+			rp('http://localhost:8080/getInvisibleInv').then(res => {
+				// 获取所有Tag成功
+				this.invList = JSON.parse(res);
+				console.log(this.invList);
+				this.$toast.clear();
+				this.$toast.success('成功');
+			}).catch(err => {
+				// 获取失败
+				console.log(err);
+				this.$toast.clear();
+				this.$toast.fail('加载失败，请检查网络连接');
+			});
+		},
+		methods: {
+			
+		}
+	}
 </script>
 
 <style>
