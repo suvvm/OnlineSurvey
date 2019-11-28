dc0572a3a85b90161cc3590063e291ceff104897
suvvm <915269675@qq.com>
2019-11-28 21:20:08 +0800

    InvController 添加处理审核请求的方法

--- OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/InvController.java
+++ OnlineSurveyBackEnd/src/main/java/qdu/suvvm/onlinesurvey/controller/InvController.java
@@ -1,7 +1,14 @@
 package qdu.suvvm.onlinesurvey.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dm.model.v20151123.SingleSendMailRequest;
+import com.aliyuncs.dm.model.v20151123.SingleSendMailResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +17,7 @@ import qdu.suvvm.onlinesurvey.mapper.InvMapper;
 import qdu.suvvm.onlinesurvey.pojo.Investigate;
 import qdu.suvvm.onlinesurvey.pojo.Tag;
 import qdu.suvvm.onlinesurvey.pojo.User;
+import qdu.suvvm.onlinesurvey.utils.EmailUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -25,6 +33,12 @@ import java.util.List;
 public class InvController {
     @Autowired
     private InvMapper invMapper;
+
+    @Value("${privateInfo.accessKeyId}")
+    private String ak_id;
+
+    @Value("${privateInfo.accessKeySecret}")
+    private String ak_secret;
 
     @PostMapping("/insertInvestigate")
     public String insertInvestigate(HttpServletRequest request) {
@@ -61,7 +75,7 @@ public class InvController {
     }
 
     @GetMapping("/getInvisibleInv")
-    public String getInvisibleInv(){
+    public String getInvisibleInv() {
         Investigate investigate = new Investigate();
         investigate.setVisible(false);
         List<Investigate> invList = invMapper.getInvestigate(investigate);
@@ -70,4 +84,37 @@ public class InvController {
         return res;
     }
 
+    @PostMapping("/getInvestigate")
+    public String getInvestigate(HttpServletRequest request) {
+        Investigate investigate = new Investigate();
+        if(request.getParameter("id") != null) {
+            investigate.setId(Integer.parseInt(request.getParameter("id")));
+        }
+        if(request.getParameter("name") != null) {
+            investigate.setName(request.getParameter("name"));
+        }
+        if(request.getParameter("ownerId") != null) {
+            User owner = new User();
+            owner.setId(Integer.parseInt(request.getParameter("ownerId")));
+            investigate.setOwner(owner);
+        }
+        List<Investigate> invList = invMapper.getInvestigate(investigate);
+        if(!invList.isEmpty()) {
+            return JSONArray.toJSONString(invList);
+        }
+        return "null";
+    }
+
+    @PostMapping("/verifyInv")
+    public void verifyInv(@RequestParam("id") String id, @RequestParam("status") String flag, @RequestParam("email") String email, @RequestParam("username") String userName, @RequestParam("ivnName") String invName) {
+        if(flag == "0"){
+            EmailUtil emailUtil = new EmailUtil();
+            emailUtil.sendEmail(email, "PopSurvey审核未通过", "尊敬的 " + userName + "，您的问卷 " + invName + " 审核未通过，请及时修改！", ak_id, ak_secret);
+        } else {
+            Investigate investigate = new Investigate();
+            investigate.setId(Integer.parseInt(id));
+            investigate.setVisible(true);
+            invMapper.updateInvestigate(investigate);
+        }
+    }
 }
