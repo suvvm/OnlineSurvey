ef06d8ab70979f6fb7a25a53a0da71a7bc7675f6
suvvm <915269675@qq.com>
2019-11-30 22:14:18 +0800

    完成关于页面

--- pages/AboutUs/AboutUs.vue
+++ pages/AboutUs/AboutUs.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		
+		<van-notice-bar
+		  text="有疑问可至本人博客页联系,或手动发送邮件至作者邮箱suvvm@foxmail.com"
+		  left-icon="volume-o"
+		/>
+		<view class="content">
+			<image class="sad" src="../../static/sadPanda.png"></image>
+			<van-button type="info" size="large" @click="toSuvvmBlog()">作者博客</van-button>
+		</view>
 	</view>
 </template>
 
@@ -12,11 +19,25 @@
 			}
 		},
 		methods: {
-			
+			toSuvvmBlog() {
+				window.location.href = 'https://www.suvvm.work/'; 
+			}
 		}
 	}
 </script>
 
 <style>
-
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.sad {
+		height: 600rpx;
+		width: 600rpx;
+		margin-top: 100rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
 </style>
