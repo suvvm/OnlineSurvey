1c5090296be0c2c53aac406249c0a9512668e925
suvvm <915269675@qq.com>
2019-12-01 19:25:15 +0800

    创建home页

---
+++ pages/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<!-- 底部导航栏 -->
+		<van-tabbar route>
+		  <van-tabbar-item icon="home-o" to="/pages/home/home">首页</van-tabbar-item>
+		  <van-tabbar-item icon="search" to="/pages/search/search">检索</van-tabbar-item>
+		  <van-tabbar-item icon="friends-o" to="/pages/categories/categories">标签</van-tabbar-item>
+		  <van-tabbar-item icon="user-o" to="/pages/user/user">我的</van-tabbar-item>
+		</van-tabbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
