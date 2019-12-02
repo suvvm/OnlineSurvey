b8d2ce9daba06d52132821fef55b9e76c6862fa2
suvvm <915269675@qq.com>
2019-12-02 22:19:20 +0800

    基本完成home页

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,13 +8,13 @@
 		
 		<van-grid class="grid">
 		  <van-grid-item class="gridItem" icon="clock-o" text="今日问卷" @click="toTodayInv()"/>
-		  <van-grid-item class="gridItem" icon="gold-coin-o" text="排行榜" @click="toRank()"/>
+		  <van-grid-item class="gridItem" icon="gold-coin-o" text="全部问卷" @click="toAllInv()"/>
 		  <van-grid-item class="gridItem" icon="todo-list-o" text="历史"  @click="toHistory()"/>
 		  <van-grid-item class="gridItem" icon="share" text="分享" @click="share()"/>
 		</van-grid>
 		<van-panel title="为我推荐">
 			<view v-if="haveRcommendInv">
-				<van-cell v-for="(item, key) in recommendInv" v-bind:key="key" :title="item.name" :value="item.statusText" size="large" :label="item.description" clickable @click="cmpInvDetails(key)" />
+				<van-cell v-for="(item, key) in recommendInv" v-bind:key="key" :title="item.name" :value="item.statusText" size="large" :label="item.description" clickable @click="toInvDetails(key)" />
 			</view>
 			<view v-else>
 				<view>暂无推荐</view>
@@ -24,7 +24,6 @@
 		<van-tabbar route>
 		  <van-tabbar-item icon="home-o" to="/pages/home/home">首页</van-tabbar-item>
 		  <van-tabbar-item icon="search" to="/pages/search/search">检索</van-tabbar-item>
-		  <van-tabbar-item icon="friends-o" to="/pages/categories/categories">标签</van-tabbar-item>
 		  <van-tabbar-item icon="user-o" to="/pages/user/user">我的</van-tabbar-item>
 		</van-tabbar>
 	</view>
@@ -46,6 +45,11 @@
 			}
 		},
 		onLoad() {
+			this.$toast.loading({
+				duration: 0,	// 持续展示 toast
+				forbidClick: true,	// 禁用背景点击
+				message: '加载中'
+			});
 			var rp = require('request-promise');
 			var options = {	// 根据id获取用户包括头像与人脸数据的全部信息
 			    method: 'POST',
@@ -99,6 +103,29 @@
 			share() {
 			        //其中915269675指的是QQ号码 可以动态获取
 			        window.location.href="mqqwpa://im/chat?chat_type=wpa&uin=915269675&version=1&src_type=web&web_src=oicqzone.com";
+			},
+			toTodayInv() {
+				this.$router.push({
+					path:'/pages/home/todayInv'
+				})
+			},
+			toAllInv() {
+				this.$router.push({
+					path:'/pages/home/allInv'
+				})
+			},
+			toHistory() {
+				this.$router.push({
+					path:'/pages/home/history'
+				})
+			},
+			toInvDetails(key) {
+				this.$router.push({
+					path: '/pages/investigates/involvedInv', 
+					query: {
+						invDetailsId: this.recommendInv[key].id
+					}
+				});
 			}
 		}
 	}
