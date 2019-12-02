2e969c0e3e46589c6de26615f4cae7a821a3442f
suvvm <915269675@qq.com>
2019-12-02 22:23:39 +0800

    修改导航栏

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -52,7 +52,6 @@
 		<van-tabbar route>
 		  <van-tabbar-item icon="home-o" to="/pages/home/home">首页</van-tabbar-item>
 		  <van-tabbar-item icon="search" to="/pages/search/search">检索</van-tabbar-item>
-		  <van-tabbar-item icon="friends-o" to="/pages/categories/categories">标签</van-tabbar-item>
 		  <van-tabbar-item icon="user-o" to="/pages/user/user">我的</van-tabbar-item>
 		</van-tabbar>
 	</view>
