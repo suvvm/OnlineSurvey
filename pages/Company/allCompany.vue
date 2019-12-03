e1f28678ec899113440639537f7fcbd4d9039149
suvvm <915269675@qq.com>
2019-12-03 20:05:31 +0800

    创建并注册页面allCompany 与allUsers

---
+++ pages/Company/allCompany.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<van-cell-group v-for="(item, key) in companies" :key="key">
+			<van-cell title="公司名" icon="coupon-o" :value="item.name"/>
+			<van-cell title="公式标签" icon="bookmark-o" >
+				<van-tag type="danger">{{item.forms}}</van-tag>/ 
+				<van-tag type="primary">{{item.domain}}</van-tag>
+			</van-cell>
+			<van-cell title="公司简介" icon="comment-o" :label="item.description"/>
+		</van-cell-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				companies: []
+			}
+		},
+		onLoad() {
+			var rp = require('request-promise');
+			var options = {	// 根据id获取用户包括头像与人脸数据的全部信息
+			    method: 'POST',
+			    uri: 'http://101.201.70.76:8211/getCompanies'
+			};
+			rp(options).then(res => {	// 请求成功
+				this.$toast.clear();
+				if(res == "error") {
+					this.$toast.fail('获取公司信息失败，未找到公司信息');
+				} else {
+					this.companies = JSON.parse(res);
+					console.log(this.companies);
+					this.$toast.success('加载成功');
+				}
+				// console.log(this.note.backgroundImage)
+			}).catch(err => {	// 请求失败
+				this.$toast.clear();
+				this.$toast.fail('加载失败, 请检查网络连接');
+				console.log(err);
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/user/allUsers.vue
@@ -0,0 +1,101 @@
+<template>
+	<view>
+		<view v-for="(item, key) in users" :key="key">
+			<van-panel :title="item.username" :desc="item.name" :status="item.email">
+			  <img :src="'data:image/png;base64,'+item.avatar" class='detail-img'/>
+			  <div slot="footer">
+			    <van-button size="small" type="danger" @click="showSendEmail()">发送邮件</van-button>
+			  </div>
+			</van-panel>
+			
+		</view>
+		<van-popup v-model="showEmail" position="bottom" :style="{ height: '50%' }" >
+			<van-field v-model="emailTitle" label="邮件标题" placeholder="请输入邮件标题"/>
+			<van-field v-model="emailBody" rows="1" autosize label="邮件内容" type="textarea" placeholder="请输入邮件内容"/>
+			<van-button size="large" type="info" @click="sendEmail()">发送</van-button>
+		</van-popup>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				users: [],
+				showEmail: false,
+				emailBody: "",
+				emailTitle: ""
+			}
+		},
+		onLoad() {
+			this.$toast.loading({
+				duration: 0,	// 持续展示 toast
+				forbidClick: true,	// 禁用背景点击
+				message: '加载中'
+			});
+			var rp = require('request-promise');
+			var options = {	// 根据id获取用户包括头像与人脸数据的全部信息
+			    method: 'POST',
+			    uri: 'http://101.201.70.76:8211/getUsers'
+			};
+			rp(options).then(res => {	// 请求成功
+				this.$toast.clear();
+				if(res == "error") {
+					this.$toast.fail('加载失败，未找到用户信息');
+				} else {
+					this.users = JSON.parse(res);
+					console.log(this.users);
+					this.$toast.success('加载成功');
+				}
+				// console.log(this.note.backgroundImage)
+			}).catch(err => {	// 请求失败
+				this.$toast.clear();
+				this.$toast.fail('加载失败, 请检查网络连接');
+				console.log(err);
+			});
+		},
+		methods: {
+			showSendEmail() {
+				this.emailTitle = ""
+				this.emailBody = "";
+				this.showEmail = true;
+			},
+			sendEmail(key) {
+				this.$toast.loading({
+					duration: 0,	// 持续展示 toast
+					forbidClick: true,	// 禁用背景点击
+					message: '发送中'
+				});
+				var rp = require('request-promise');
+				var options = {	// 根据id获取用户包括头像与人脸数据的全部信息
+				    method: 'POST',
+				    uri: 'http://101.201.70.76:8211/sendEmail',
+					form: {
+					    email: this.users[key].email,
+						title: this.emailTitle,
+						body: this.emailBody
+					}
+				};
+				rp(options).then(res => {	// 请求成功
+					this.showEmail = false;
+					this.$toast.clear();
+					this.$toast.success('发送成功');
+
+					// console.log(this.note.backgroundImage)
+				}).catch(err => {	// 请求失败
+					this.$toast.clear();
+					this.$toast.fail('发送失败, 请检查网络连接');
+					console.log(err);
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.detail-img {
+		width: 280rpx;
+		height: 280rpx;
+		margin-left: 24rpx;
+	}
+</style>
