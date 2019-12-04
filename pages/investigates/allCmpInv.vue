0077fa47abc07ae0c2fc7fa4d2676a58ae516b08
suvvm <915269675@qq.com>
2019-12-04 15:48:51 +0800

    allCmpInv添加下拉刷新方法

--- pages/investigates/allCmpInv.vue
+++ pages/investigates/allCmpInv.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		<van-cell v-for="(item, key) in userInfo.investigates" v-bind:key="key" :title="item.name" :value="item.statusText" size="large" :label="item.description" clickable @click="cmpInvDetails(key)" />
+		<van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+			<van-cell v-for="(item, key) in userInfo.investigates" v-bind:key="key" :title="item.name" :value="item.statusText" size="large" :label="item.description" clickable @click="cmpInvDetails(key)" />
+		</van-pull-refresh>
 	</view>
 </template>
 
@@ -8,7 +10,8 @@
 	export default {
 		data() {
 			return {
-				userInfo: {}
+				userInfo: {},
+				isLoading: false
 			}
 		},
 		onLoad() {
@@ -66,6 +69,42 @@
 						invId: this.userInfo.investigates[key].id
 					}
 				});
+			},
+			onRefresh() {
+				setTimeout(() => {
+					var rp = require('request-promise');
+					var options = {
+					    method: 'POST',
+					    uri: 'http://101.201.70.76:8211/getUserById',
+					    form: {
+					        id: this.$cookies.get("userInfo").id,
+					    }
+					};
+					rp(options).then(res => {
+						this.isLoading = false;
+						if(res == "error") {
+							this.$toast.fail('获取用户信息识别，未找到用户信息');
+						} else {
+							this.userInfo = JSON.parse(res);
+							console.log(this.userInfo);
+							for(var i = 0; i < this.userInfo.investigates.length; i++){
+								if(this.userInfo.investigates[i].visible){
+									this.userInfo.investigates[i].statusText = "已审核";
+								} else {
+									this.userInfo.investigates[i].statusText = "未审核";
+								}
+								
+							}
+							this.$forceUpdate();
+							this.$toast.success('刷新成功');
+						}
+						// console.log(this.note.backgroundImage)
+					}).catch(err => {
+						this.isLoading = false;
+						this.$toast.fail('获取用户信息, 请检查网络连接');
+						console.log(err);
+					});
+				}, 500);
 			}
 		}
 	}
