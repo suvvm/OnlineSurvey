ef37a386de7ec43e5f6f6bf8a69a5150c0e22684
suvvm <915269675@qq.com>
2019-12-04 15:49:25 +0800

    allUsers添加排序方法

--- pages/user/allUsers.vue
+++ pages/user/allUsers.vue
@@ -4,7 +4,8 @@
 			<van-panel :title="item.username" :desc="item.name" :status="item.email">
 			  <img :src="'data:image/png;base64,'+item.avatar" class='detail-img'/>
 			  <div slot="footer">
-			    <van-button size="small" type="danger" @click="showSendEmail()">发送邮件</van-button>
+			    <van-button size="small" type="danger" @click="showSendEmail(key)">发送邮件</van-button>
+				<van-button size="small" type="info" @click="userEOP(key)">提升权限</van-button>
 			  </div>
 			</van-panel>
 			
@@ -24,7 +25,8 @@
 				users: [],
 				showEmail: false,
 				emailBody: "",
-				emailTitle: ""
+				emailTitle: "",
+				nowKey: 0
 			}
 		},
 		onLoad() {
@@ -55,29 +57,34 @@
 			});
 		},
 		methods: {
-			showSendEmail() {
-				this.emailTitle = ""
+			showSendEmail(key) {
+				this.nowKey = key;
+				this.emailTitle = "";
 				this.emailBody = "";
 				this.showEmail = true;
 			},
-			sendEmail(key) {
+			sendEmail() {
 				this.$toast.loading({
 					duration: 0,	// 持续展示 toast
 					forbidClick: true,	// 禁用背景点击
 					message: '发送中'
 				});
+				// console.log("发送邮件");
+				// console.log(this.nowKey);
+				// console.log(this.users[this.nowKey].email);
 				var rp = require('request-promise');
 				var options = {	// 根据id获取用户包括头像与人脸数据的全部信息
 				    method: 'POST',
 				    uri: 'http://101.201.70.76:8211/sendEmail',
 					form: {
-					    email: this.users[key].email,
+					    email: this.users[this.nowKey].email,
 						title: this.emailTitle,
 						body: this.emailBody
 					}
 				};
 				rp(options).then(res => {	// 请求成功
 					this.showEmail = false;
+					console.log(res);
 					this.$toast.clear();
 					this.$toast.success('发送成功');
 
@@ -86,6 +93,33 @@
 					this.$toast.clear();
 					this.$toast.fail('发送失败, 请检查网络连接');
 					console.log(err);
+				});
+			},
+			userEOP(key) {
+				this.$toast.loading({
+					duration: 0,	// 持续展示 toast
+					forbidClick: true,	// 禁用背景点击
+					message: '提交中'
+				});
+
+				var rp = require('request-promise');
+				var options = {	// 提升用户权限
+				    method: 'POST',
+				    uri: 'http://101.201.70.76:8211/userEOP',
+					form: {
+						id: this.users[key].id,
+						power: 2
+					}
+				};
+				
+				rp(options).then(res => {	// 请求成功
+					// console.log(res);
+					this.$toast.clear();
+					this.$toast.success('提升成功');
+				}).catch(err => {	// 请求失败
+					this.$toast.clear();
+					this.$toast.fail('提升失败');
+					// console.log(err);
 				});
 			}
 		}
