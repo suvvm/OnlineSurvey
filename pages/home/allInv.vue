d993f051400fd43921da40efa6e3cbe537c4203c
suvvm <915269675@qq.com>
2019-12-10 10:19:58 +0800

    allInv 注释控制台打印方法

--- pages/home/allInv.vue
+++ pages/home/allInv.vue
@@ -28,40 +28,40 @@
 				message: '加载中'
 			});
 			var rp = require('request-promise');
-			rp('http://101.201.70.76:8211/getVisibleInv').then(res => {	// 请求成功
+			rp('http://101.201.70.76:8211/getVisibleInv').then(res => {	// 请求获取所有可见问卷
 				this.$toast.clear();
 				if(res == "error") {
 					this.$toast.fail('获取问卷失败，未找到问卷信息');
 				} else {
 					this.investigates = JSON.parse(res);
-					console.log(this.investigates);
+					// console.log(this.investigates);
 					this.$toast.success('加载成功');
 				}
 				// console.log(this.note.backgroundImage)
 			}).catch(err => {	// 请求失败
 				this.$toast.clear();
 				this.$toast.fail('获取问卷信息, 请检查网络连接');
-				console.log(err);
+				// console.log(err);
 			});
 		},
 		methods: {
-			onPaginationChanged() {
+			onPaginationChanged() {	// 翻页函数
 				this.listBg = this.currentPage * 5 - 5;
 			},
-			toInvDetails(key) {
-				this.$router.push({
+			toInvDetails(key) {	// 根据问卷id跳转至对应答题页
+				this.$router.push({	
 					path: '/pages/investigates/involvedInv', 
 					query: {
 						invDetailsId: this.investigates[key].id
 					}
 				});
 			},
-			 onConfirm(value, index) {
+			 onConfirm(value, index) {	// 排序函数
 				// console.log(value);
-				if(value == "时间") {
+				if(value == "时间") {	// 根据时间排序
 					this.investigates.sort(function(a,b){
 					return a.time>b.time});
-				}else if(value == "参与人数") {
+				}else if(value == "参与人数") {	// 根据参与人数排序
 					this.investigates.sort(function(a,b){
 					return a.results.length < b.results.length});
 				}
